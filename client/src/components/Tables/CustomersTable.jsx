2868dfcda009dee33d793273cb0a7588b1b9897f
Fathi Abdi <fathiabdi254@gmail.com>
2024-12-23 20:40:07 +0300

    You can see a loading spinner before showing customer, transactions and invoices table data

--- client/src/components/Tables/CustomersTable.jsx
+++ client/src/components/Tables/CustomersTable.jsx
@@ -1,13 +1,23 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import { ClipLoader } from "react-spinners";
 
 function CustomersTable() {
-  const [customers, setCustomers] = useState([])
+  const [customers, setCustomers] = useState([]);
+  const [loading, setLoading] = useState(true); // Track loading state
+
   useEffect(() => {
-    axios.get('http://localhost:3001/api/customers')
-    .then(customer => setCustomers(customer.data))
-    .catch(err => console.log(err))
-  }, [])
+    axios
+      .get("http://localhost:3001/api/customers")
+      .then((response) => {
+        setCustomers(response.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <div>
@@ -23,14 +33,24 @@ function CustomersTable() {
 
         {/* Table Data */}
         <tbody className="bg-blue-50">
-          { customers.map(customer => {
-            return (
-          <tr className="border border-t border-b border-l-0 border-r-0 border-[#0B81C7] border-opacity-10 hover:bg-blue-100">
-            <td className="py-2 px-2 text-sm">{customer.fullName}</td>
-            <td className="py-2 px-2 text-sm">{customer.email}</td>
-            <td className="py-2 px-2 text-sm">{customer.age}</td>
-          </tr>
-          )})}
+          {loading ? (
+            <tr>
+              <td colSpan="3" className="py-4 text-center">
+                <ClipLoader color="#0B81C7" loading={loading} size={30} />
+              </td>
+            </tr>
+          ) : (
+            customers.map((customer) => (
+              <tr
+                key={customer.id}
+                className="border border-t border-b border-l-0 border-r-0 border-[#0B81C7] border-opacity-10 hover:bg-blue-100"
+              >
+                <td className="py-2 px-2 text-sm">{customer.fullName}</td>
+                <td className="py-2 px-2 text-sm">{customer.email}</td>
+                <td className="py-2 px-2 text-sm">{customer.age}</td>
+              </tr>
+            ))
+          )}
         </tbody>
       </table>
     </div>
